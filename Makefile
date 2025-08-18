85a5659935dc3afdf7b300dc84ed54db38a18519
Emma Bateman <emmalbateman@gmail.com>
2025-08-17 18:53:06 -0700

    GUI for navigating palindromes more quickly

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 palindromer:
-	g++ -std=c++17 cmdLine.cpp trie.cpp palindrome.cpp -o Palindromer
+	g++ -std=c++17 main.cpp cmdLine.cpp trie.cpp -o Palindromer
+palindromer-visual:
+	g++ -std=c++17 visualUI.cpp trie.cpp `wx-config --libs --cxxflags` -o PalindromerVisual
---
+++ visualUI.h
@@ -0,0 +1,61 @@
+#include <deque>
+#include <thread>
+#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/settings.h>
+#include <wx/xrc/xmlres.h>
+#include <wx/xrc/xh_bmp.h>
+#include <wx/frame.h>
+#include <wx/iconbndl.h>
+#include <wx/artprov.h>
+#include <wx/sizer.h>
+#include <wx/panel.h>
+#include <wx/textctrl.h>
+#include <wx/button.h>
+#include <wx/listctrl.h>
+
+class VisualUI : public wxApp {
+    public:
+        bool OnInit() override;
+};
+
+class Frame : public wxFrame {
+    public:
+        Frame(wxWindow* parent,
+              wxWindowID id = wxID_ANY,
+              const wxString& title = _("Palindromer"),
+              const wxPoint& pos = wxDefaultPosition,
+              const wxSize& size = wxSize(500,300),
+              long style = wxCAPTION|wxRESIZE_BORDER|wxMAXIMIZE_BOX|wxMINIMIZE_BOX|wxSYSTEM_MENU|wxCLOSE_BOX);
+        virtual ~Frame();
+        
+        void OnDictionaryLoaded(bool success);
+    
+    protected:
+        wxTextCtrl* forTextCtrl;
+        wxTextCtrl* bacTextCtrl;
+        wxTextCtrl* palindromeTextCtrl;
+        wxButton* backButton;
+        wxButton* fwdButton;
+        wxButton* searchButton;
+        wxListCtrl* listCtrl;
+        wxButton* prevPageButton;
+        wxButton* nextPageButton;
+    
+    private:
+        void OnSearch(wxCommandEvent&);
+        void OnItemSelected(wxListEvent& event);
+        void OnBack(wxCommandEvent& event);
+        void OnForward(wxCommandEvent& event);
+        void PrevPage(wxCommandEvent& event);
+        void NextPage(wxCommandEvent& event);
+        void Search();
+        void DisplayList();
+
+        std::deque<std::pair<std::string, std::string>> backStack;
+        std::deque<std::pair<std::string, std::string>> fwdStack;
+        std::string startFor;
+        std::string startBac;
+        std::vector<std::string> resultList;
+        int pageNo;
+};
